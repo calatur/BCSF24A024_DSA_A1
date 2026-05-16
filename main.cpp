5def7a19a5ad9ec31ba240d93ea27abb31ee9947
Muhammad Ali <m.alishahzad95@gmail.com>
2026-05-16 20:45:25 +0500

    Basic definitions and Precedence function
    added basic definitions and a precedence function for operators

--- main.cpp
+++ main.cpp
@@ -5,12 +5,35 @@
 
 using namespace std;
 
-bool validate();
+bool validate(string& in);
 int eval(string& in);
+int precedence(char op);
 
 int main(){
     string in;
     cout << "Enter the expression: ";
     getline(cin, in);
     return eval(in);
+}
+
+bool validate(){
+
+    return true;
+}
+
+int eval(string& in){
+    vector<string> variables;
+    vector<int> tokens;
+    return 0;
+}
+
+int precedence (char op){
+    int res = 0;
+    if (op == '+' || op == '-'){
+        res = 1;
+    }
+    else if (op == '*' || op == '/'){
+        res = 2;
+    }
+    return res;
 }
